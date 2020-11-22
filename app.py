ca4431c73e404b04133c09a652be52e707ab44da
blue-hage <mahi.sakou.011047@gmail.com>
2020-11-23 05:10:00 +0900

    revise app.py

--- app.py
+++ app.py
@@ -7,5 +7,21 @@ app.secret_key = "blublunomi_gomugomu"
 def index():
   return render_template("layout.html")
 
+@app.route("/concept")
+def concept():
+  return render_template("concept.html")
+
+@app.route("/service")
+def service():
+  return render_template("service.html")
+
+@app.route("/aboutus")
+def aboutus():
+  return render_template("aboutus.html")
+
+@app.route("/contact")
+def contact():
+  return render_template("contact.html")
+
 if __name__ == "__main__":
   app.run(debug=True)
