da5c247466be53e0fe87150366fde84722563d92
blue-hage <mahi.sakou.011047@gmail.com>
2020-12-01 09:12:32 +0900

    contact button is not working yet

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,9 @@
 function check(){
   const check_box = document.getElementById("check");
-  const button = document.getElementById("submit")
+  const button = document.getElementById("button")
   if(check_box.checked){
     button.disabled = false;
   }else{
-    button.disabled = true
+    button.disabled = true;
   }
-}
+};
