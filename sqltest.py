13f0b42bb0a8c87baa91b098f010dcb223d18d92
blue-hage <mahi.sakou.011047@gmail.com>
2020-12-13 18:43:10 +0900

    DBtest

---
+++ sqltest.py
@@ -0,0 +1,9 @@
+import mysql.connector
+config = {
+  'host':'localhost',
+  'user':'bluroom_client',
+  'password':'20201213',
+  'database':'bluroom_client'
+}
+conn = mysql.connector.connect(**config)
+print(conn.is_connected())
