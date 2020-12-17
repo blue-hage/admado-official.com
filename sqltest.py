9d950e285bee067e3f4b91cdb7b16be633c10ce3
blue-hage <mahi.sakou.011047@gmail.com>
2020-12-17 13:42:58 +0900

    test

---
+++ sqltest.py
@@ -0,0 +1,20 @@
+#!/usr/local/bin/python3
+import mysql.connector
+
+config = {
+  'host':'localhost',
+  'user':'bluroom_client',
+  'password':'20201213',
+  'database':'bluroom_client'
+}
+
+company_id = "adMado"
+user_id = "太郎"
+filename = "admado.png"
+conn = mysql.connector.connect(**config)
+mycursor = conn.cursor()
+sql = 'INSERT INTO test (file_id, company_id, user_id, filename, created_at) VALUES (NULL, %s, %s, %s, CURRENT_TIMESTAMP)'
+val = (company_id, user_id, filename)
+mycursor.execute(sql, val)
+conn.commit()
+conn.close()
