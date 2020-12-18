f2bc29f07e6c9774d2f7ed28e5e12d79017c7f75
blue-hage <mahi.sakou.011047@gmail.com>
2020-12-18 18:44:29 +0900

    test one organized some

--- client.py
+++ client.py
@@ -11,12 +11,12 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 def allowed_file(filename):
   return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def save_file(name, design):
+def save_file(name, design, company_id):
   if name == "No":
     return "no file"
 
   if design and allowed_file(design.filename):
-    picId = sql.select('SELECT * FROM test WHERE filename=%s', name)
+    picId = sql.select('SELECT * FROM test WHERE filename = %s AND company_id = %s', name, company_id)
     filename = picId[0] + "_" + name
     design.save(os.path.join(FILES_DIR, filename))
     return FILES_DIR + '/' + filename
