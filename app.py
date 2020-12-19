ccc0aaab7433e03618066ded13de96de93d0c732
blue-hage <mahi.sakou.011047@gmail.com>
2020-12-19 20:15:46 +0900

    test for get database

--- app.py
+++ app.py
@@ -146,7 +146,7 @@ def admin_login():
 @app.route("/admin/client/list/secret")
 @admin.login_required
 def admin_client():
-  clients = sql.select("SELECT * FROM test")
+  clients = sql.exec("SELECT * FROM test")
   return render_template("client_list.html", client_list=clients)
 
 if __name__ == "__main__":
